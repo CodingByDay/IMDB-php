07523d1a7dc5df1966b80afb975ae67cba1204ce
Janko <jankoovicic351@gmail.com>
2021-04-02 09:42:26 +0200

    Testing php

--- index.php
+++ index.php
@@ -6,6 +6,15 @@
 </head>
 <body>
 <h1>Welcome to the page.</h1>
+<?php
+$b = 110;
+
+echo $b;
+
+?>
+
+
+<a href="movies.php?vrednost=105">Movies</a>
 </body>
 
 </html>
---
+++ movies.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+
+
+
+</head>
+<body>
+<h1>Welcome to movies</h1>
+<?php
+
+
+
+
+
+$a = $_GET['vrednost'];
+echo $a;
+
+?>
+<a href="index.php">Movies</a>
+
+</body>
+
+</html>
